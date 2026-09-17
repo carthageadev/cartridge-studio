ff65458c4037ca503c8c689151edf61530138d81
carthageadev <AhmedDev@email.com>
2026-09-17 06:24:42 +0100

    fix server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { buildUpstreamQuery, credsFromEnv, sanitizeSsJson } from './api/_ss';
+import { buildUpstreamQuery, credsFromEnv, sanitizeSsJson } from './api/ss';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
